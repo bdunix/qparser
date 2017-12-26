cd7976d152cf14ce728614acfe2c6ee84dd8e77c
bdu <bdunix@gmail.com>
2017-12-26 14:52:27 +0800

    Add QSettings and Change CRLF to LF

--- qparser.py
+++ qparser.py
@@ -16,12 +16,16 @@ class QParser(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
         settings = QSettings()
-        self.parserfolder_path = settings.value("parserfolder_path") or "C:\\work\\tools\\linux-ramdump-parser-v2\\"
-        self.toolsfolder_path = settings.value("toolsfolder_path") or "C:\\work\\tools\\"
-
-        self.dumpfolder_path = settings.value("dumpfolder_path") or "E:\\"
-        self.vmlinux_path = os.path.join(self.dumpfolder_path, "vmlinux")
-        self.outputfolder_path = os.path.join(self.dumpfolder_path, "parser\\")
+        self.parserfolder_path = settings.value("parserfolder_path") \
+                                 or "C:\\work\\tools\\linux-ramdump-parser-v2"
+        self.toolsfolder_path = settings.value("toolsfolder_path") \
+                                or "C:\\work\\tools"
+        self.dumpfolder_path = settings.value("dumpfolder_path") \
+                               or "E:\\"
+        self.vmlinux_path = settings.value("vmlinux_path") \
+                            or os.path.join(self.dumpfolder_path, "vmlinux")
+        self.outputfolder_path = settings.value("outputfolder_path") \
+                                 or os.path.join(self.dumpfolder_path, "parser")
 
         self.update_ui()
 
@@ -32,58 +36,74 @@ class QParser(QMainWindow, Ui_MainWindow):
         self.vmlinuxLineEdit.setText(self.vmlinux_path)
         self.outputfolderLineEdit.setText(self.outputfolder_path)
 
-    @pyqtSlot()
-    def dumpfolderLineEdit_textChanged(self):
-        self.vmlinux_path = os.path.join(self.dumpfolder_path, "vmlinux")
-        self.outputfolder_path = os.path.join(self.dumpfolder_path, "parser\\")
+    def get_folder_path(self, folder_path):
+        dir = os.path.dirname(folder_path if folder_path is not None else ".")
+        path = QFileDialog.getExistingDirectory(self, "Open folder", dir)
+        return QDir.toNativeSeparators(path)
 
+    def get_file_path(self, file_path):
+        dir = os.path.dirname(file_path if file_path is not None else ".")
+        path = QFileDialog.getOpenFileName(self, "Open file", dir)
+        path = path[0]
+        return QDir.toNativeSeparators(path)
 
     @pyqtSlot()
     def on_parserfolderPushButton_clicked(self):
-        dir = os.path.dirname(self.parserfolder_path if self.parserfolder_path is not None else ".")
-        path = QFileDialog.getExistingDirectory(self, "Open parser folder", dir)
+        path = self.get_folder_path(self.parserfolder_path)
         if path:
             self.parserfolder_path = path
-            self.update_ui()
-            #self.parserfolderLineEdit.setText(self.parserfolder_path)
+            self.parserfolderLineEdit.setText(path)
 
     @pyqtSlot()
     def on_toolsfolderPushButton_clicked(self):
-        dir = os.path.dirname(self.toolsfolder_path if self.toolsfolder_path is not None else ".")
-        path = QFileDialog.getExistingDirectory(self, "Open tools((gdb/nm/objdump)) folder", dir)
+        path = self.get_folder_path(self.toolsfolder_path)
         if path:
             self.toolsfolder_path = path
-            self.toolsfolderLineEdit.setText(self.toolsfolder_path)
+            self.toolsfolderLineEdit.setText(path)
 
     @pyqtSlot()
     def on_dumpfolderPushButton_clicked(self):
-        dir = os.path.dirname(self.dumpfolder_path if self.dumpfolder_path is not None else ".")
-        path = QFileDialog.getExistingDirectory(self, "Open dump folder", dir)
+        path = self.get_folder_path(self.dumpfolder_path)
         if path:
             self.dumpfolder_path = path
-            self.dumpfolderLineEdit.setText(self.dumpfolder_path)
+            self.dumpfolderLineEdit.setText(path)
 
     @pyqtSlot()
     def on_vmlinuxPushButton_clicked(self):
-        dir = os.path.dirname(self.vmlinux_path if self.vmlinux_path is not None else ".")
-        path = QFileDialog.getOpenFileName(self, "Open vmlinux", dir)[0]
+        path = self.get_file_path(self.vmlinux_path)
         if path:
             self.vmlinux_path = path
-            self.vmlinuxLineEdit.setText(self.vmlinux_path)
+            self.vmlinuxLineEdit.setText(path)
 
     @pyqtSlot()
     def on_outputfolderPushButton_clicked(self):
-        dir = os.path.dirname(self.outputfolder_path if self.outputfolder_path is not None else ".")
-        path = QFileDialog.getExistingDirectory(self, "Open output folder", dir)
+        path = self.get_folder_path(self.outputfolder_path)
         if path:
             self.outputfolder_path = path
-            self.outputfolderLineEdit.setText(self.outputfolder_path)
+            self.outputfolderLineEdit.setText(path)
+
+    @pyqtSlot("QString")
+    def on_dumpfolderLineEdit_textChanged(self, arg1):
+        self.vmlinux_path = os.path.join(arg1, "vmlinux")
+        self.outputfolder_path = os.path.join(arg1, "parser")
+        self.update_ui()
+
+    def closeEvent(self, *args, **kwargs):
+        settings = QSettings()
+        self.parserfolder_path = settings.setValue("parserfolder_path", self.parserfolder_path)
+        self.toolsfolder_path = settings.setValue("toolsfolder_path", self.toolsfolder_path)
+        self.dumpfolder_path = settings.setValue("dumpfolder_path", self.dumpfolder_path)
+        self.vmlinux_path = settings.setValue("vmlinux_path", self.vmlinux_path)
+        self.outputfolder_path = settings.setValue("outputfolder_path", self.outputfolder_path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setOrganizationName("bdu ltd.")
+    app.setOrganizationDomain("bdu.org")
+    app.setApplicationName("QParser")
 
-    mainwin = QParser()
-    mainwin.show()
+    m = QParser()
+    m.show()
 
     sys.exit(app.exec_())
