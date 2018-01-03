501f83517e7c7c7f456e6b4b07188c3051cddd2b
bdu <bdunix@gmail.com>
2018-01-03 17:25:15 +0800

    Work on linux

--- qparser.py
+++ qparser.py
@@ -4,6 +4,7 @@
 import subprocess
 import os
 import sys
+import platform
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
@@ -16,36 +17,59 @@ class QParser(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
 
+        self.load_settings()
+
+        self.update_ui()
+
+    def load_settings(self):
+        if platform.system() == 'Linux':
+            self.python_path = "/usr/bin/python2"
+            parserfolder = "/opt/tools/linux-ramdump-parser-v2"
+            toolsfolder = "/opt/LinaroToolchain/gcc-linaro-4.9.4-2017.01-x86_64_aarch64-elf/bin"
+            dumpfolder = "~/case"
+            gdb64 = "aarch64-elf-gdb"
+            nm64 = "aarch64-elf-nm"
+            objdump64 = "aarch64-elf-objdump"
+            gdb = "arm-eabi-gdb"
+            nm = "arm-eabi-nm"
+            objdump = "arm-eabi-objdump"
+
+        if platform.system() == 'Windows':
+            self.python_path = "C:\Python27\python.exe"
+            parserfolder = "C:\\work\\tools\\linux-ramdump-parser-v2"
+            toolsfolder = "C:\\work\\tools"
+            dumpfolder = "E:\\"
+            gdb64 = "aarch64-linux-gnu-gdb.exe"
+            nm64 = "aarch64-linux-gnu-gcc-nm.exe"
+            objdump64 = "aarch64-linux-gnu-objdump.exe"
+            gdb = "arm-none-eabi-gdb.exe"
+            nm = "arm-none-eabi-nm.exe"
+            objdump = "arm-none-eabi-objdump.exe"
+
         settings = QSettings()
-        self.parserfolder_path = settings.value("parserfolder_path") \
-                                 or "C:\\work\\tools\\linux-ramdump-parser-v2"
-        self.toolsfolder_path = settings.value("toolsfolder_path") \
-                                or "C:\\work\\tools"
-        self.dumpfolder_path = settings.value("dumpfolder_path") \
-                               or "E:\\"
+        self.parserfolder_path = settings.value("parserfolder_path") or parserfolder
+        self.parser_path = os.path.join(self.parserfolder_path, "ramparse.py")
+        self.toolsfolder_path = settings.value("toolsfolder_path") or toolsfolder
+        self.gdb64_path = os.path.join(self.toolsfolder_path, gdb64)
+        self.nm64_path = os.path.join(self.toolsfolder_path, nm64)
+        self.objdump64_path = os.path.join(self.toolsfolder_path, objdump64)
+        self.gdb_path = os.path.join(self.toolsfolder_path, gdb)
+        self.nm_path = os.path.join(self.toolsfolder_path, nm)
+        self.objdump_path = os.path.join(self.toolsfolder_path, objdump)
+        self.dumpfolder_path = settings.value("dumpfolder_path") or dumpfolder
         self.vmlinux_path = settings.value("vmlinux_path") \
                             or os.path.join(self.dumpfolder_path, "vmlinux")
         self.outputfolder_path = settings.value("outputfolder_path") \
                                  or os.path.join(self.dumpfolder_path, "parser")
+        self.hardware = self.forcehwLineEdit.text()
 
-        self.python_path = "C:\Python27\python.exe"
-        parser = "ramparse.py"
-        self.parser_path = os.path.join(self.parserfolder_path, parser)
-
-        self.update_tools_path()
-
-        self.hardware = "660"
-
-
-        self.update_ui()
-
-    def update_tools_path(self):
-        gdb64 = "aarch64-linux-gnu-gdb.exe"
-        nm64 = "aarch64-linux-gnu-gcc-nm.exe"
-        objdump64 = "aarch64-linux-gnu-objdump.exe"
-        self.gdb64_path = os.path.join(self.toolsfolder_path, gdb64)
-        self.nm64_path = os.path.join(self.toolsfolder_path, nm64)
-        self.objdump64_path = os.path.join(self.toolsfolder_path, objdump64)
+    def save_settings(self):
+        settings = QSettings()
+        self.parserfolder_path = settings.setValue("parserfolder_path", self.parserfolder_path)
+        self.toolsfolder_path = settings.setValue("toolsfolder_path", self.toolsfolder_path)
+        self.dumpfolder_path = settings.setValue("dumpfolder_path", self.dumpfolder_path)
+        self.vmlinux_path = settings.setValue("vmlinux_path", self.vmlinux_path)
+        self.outputfolder_path = settings.setValue("outputfolder_path", self.outputfolder_path)
 
     def update_ui(self):
         self.parserfolderLineEdit.setText(self.parserfolder_path)
@@ -64,6 +88,18 @@ class QParser(QMainWindow, Ui_MainWindow):
         path = QFileDialog.getOpenFileName(self, "Open file", dir)
         path = path[0]
         return QDir.toNativeSeparators(path)
+
+    def run_parser_subprocess(self, args):
+        p = subprocess.Popen(args, stdout=subprocess.PIPE)
+        while p.poll() is None:
+            line = p.stdout.readline()
+            line = line.strip()
+            if line:
+                print('Subprocess output: [{}]'.format(line))
+        if p.returncode == 0:
+            print('Subprocess success')
+        else:
+            print('Subprocess failed')
 
     @pyqtSlot()
     def on_parserfolderPushButton_clicked(self):
@@ -107,36 +143,22 @@ class QParser(QMainWindow, Ui_MainWindow):
         self.outputfolder_path = os.path.join(arg, "parser")
         self.update_ui()
 
-    def run_parser_subprocess(self):
-        p = subprocess.run(self.cmdline)
-        while p.poll() is None:
-            line = p.stdout.readline()
-            line = line.strip()
-            if line:
-                print('Subprogram output: [{}]'.format(line))
-        if p.returncode == 0:
-            print('Subprogram success')
-        else:
-            print('Subprogram failed')
-
     @pyqtSlot()
     def on_parsePushButton_clicked(self):
-        options = [self.python_path, self.parser_path, "-v", self.vmlinux_path, "-g", self.gdb64_path, \
-                       "-n", self.nm64_path, "-j", self.objdump64_path, "-o", self.outputfolder_path, \
-                       "-a", self.dumpfolder_path, "--force-hardware", self.hardware, "-x"]
-        self.cmdline = " ".join(options)
-        self.outputTextEdit.setText(self.cmdline)
+        args = [self.python_path, self.parser_path, "-v", self.vmlinux_path, "-g", self.gdb64_path, \
+                "-n", self.nm64_path, "-j", self.objdump64_path, "-o", self.outputfolder_path, \
+                "-a", self.dumpfolder_path, "-x"]
+        if self.forcehwCheckBox.isChecked():
+            args += ["--force-hardware", self.hardware]
+        self.outputTextEdit.setText(" ".join(args))
+        self.run_parser_subprocess(args)
 
-        self.run_parser_subprocess()
+    @pyqtSlot()
+    def on_forcehwLineEdit_textChanged(self, arg):
+        self.hardware = self.forcehwLineEdit.text()
 
-
-def closeEvent(self, *args, **kwargs):
-        settings = QSettings()
-        self.parserfolder_path = settings.setValue("parserfolder_path", self.parserfolder_path)
-        self.toolsfolder_path = settings.setValue("toolsfolder_path", self.toolsfolder_path)
-        self.dumpfolder_path = settings.setValue("dumpfolder_path", self.dumpfolder_path)
-        self.vmlinux_path = settings.setValue("vmlinux_path", self.vmlinux_path)
-        self.outputfolder_path = settings.setValue("outputfolder_path", self.outputfolder_path)
+    def closeEvent(self, *args, **kwargs):
+        self.save_settings()
 
 
 if __name__ == "__main__":
